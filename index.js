4bff9a0128f956a1a69d8b3aabe7a2534ab401ca
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-08-19 20:54:35 +0300

    complete assignment

--- index.js
+++ index.js
@@ -9,3 +9,41 @@ function ternaryCheckCity(){
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+
+
+
+
+function scuberGreetingForFeet(value) {
+  
+  if (value <= 400) {
+    return "This one is on me!";
+  } else if (value > 2000 && value <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+      return 'Ok, sounds good.'
+  } else {
+      return 'No go.'
+  }
+}
+
+function switchOnCharmFromTip(next) {
+  
+  switch (next) {
+    case 'generous':
+      return 'Thank you so much.';
+
+    case 'not as generous':
+      return 'Thank you.';
+
+
+
+    default:
+      return 'Bye.';
+  }
+}
